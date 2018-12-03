6fd07b673116a28ab4c18b4a93ccc3eedd2e5f61
nihelmdj <45205090+nihelmdj@users.noreply.github.com>
2018-12-03 19:59:24 +0100

    jcomprend rien

---
+++ Voyage²A/Oualler.js
@@ -0,0 +1,14 @@
+
+function progression() {
+    var elem = document.getElementById("bar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+        }
+    }
+}
